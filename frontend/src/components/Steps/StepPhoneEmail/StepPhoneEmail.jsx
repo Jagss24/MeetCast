eb6764d15d74f9cf2b2eb3e663442fb922768bad
Jagss24 <btwitsjagannath12@gmail.com>
2024-05-21 23:20:05 +0530

    Made the UI of StepPhoneEmail with styled-components and rendered phone & email form  based on conditions

--- frontend/src/components/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ frontend/src/components/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -1,27 +1,56 @@
 import React, { useState } from 'react'
 import { MainStyled, CardStyled, HeadingStyled, HeadingWrapper, HeadingImg, ButtonWrapper } from "../../shared/commonStyles/Card.styled"
-import "./StepPhoneEmail.styled.css"
+import { TermStyled, InputBoxStyled, InputStyled, EmailPhoneWrapper } from "./StepPhoneEmail.styled"
+import { CiMobile1 } from "react-icons/ci";
+import { AiOutlineMail } from "react-icons/ai";
 const StepPhoneEmail = () => {
     const [number, setNumber] = useState()
+    const [email, setEmail] = useState()
+    const [isPhoneActive, setIsPhoneActive] = useState(true)
     return (
         <>
-            <MainStyled>
+            <EmailPhoneWrapper>
+                <CiMobile1 size={24} style={{
+                    background: isPhoneActive ? "#0077ff" : "#262626", borderRadius: "8px", padding: "4px",
+                    cursor: "pointer"
+                }} onClick={() => setIsPhoneActive(prev => !prev)} />
+                <AiOutlineMail size={24} style={{
+                    background: isPhoneActive ? "#262626" : "#0077ff", borderRadius: "8px", padding: "4px",
+                    cursor: "pointer"
+                }} onClick={() => setIsPhoneActive(prev => !prev)} />
+            </EmailPhoneWrapper>
+            {isPhoneActive ? <MainStyled>
                 <CardStyled>
                     <HeadingWrapper>
-                        <HeadingImg src='/images/tel.png'></HeadingImg>
+                        <HeadingImg src='/images/tel.png' style={{ width: "25px", height: "25px" }}></HeadingImg>
                         <HeadingStyled>Enter your Phone number</HeadingStyled>
                     </HeadingWrapper>
-                    <div className='inputBoxWrapper'>
+                    <InputBoxStyled className='inputBoxWrapper'>
                         <img src='/images/india.png' alt="india_logo" />
-                        <input type="number" placeholder='8369198662' value={number} onChange={e => setNumber(e.target.value)} />
-                    </div>
+                        <InputStyled type="number" placeholder='8369198662' value={number} onChange={e => setNumber(e.target.value)} />
+                    </InputBoxStyled>
                     <ButtonWrapper>
                         Next
                         <img src='/images/arrow_forward.png' />
                     </ButtonWrapper>
-                    <span className='terms'>By entering your number you are agreeing to our Terms of services and Privacy Policy Thanks!</span>
+                    <TermStyled>By entering your number you are agreeing to our Terms of services and Privacy Policy Thanks!</TermStyled>
                 </CardStyled>
-            </MainStyled>
+            </MainStyled> : <MainStyled>
+                <CardStyled>
+                    <HeadingWrapper>
+                        <HeadingImg src='/images/msg.png' style={{ width: "25px", height: "20px" }}></HeadingImg>
+                        <HeadingStyled>Enter your Email Id</HeadingStyled>
+                    </HeadingWrapper>
+                    <InputBoxStyled className='inputBoxWrapper'>
+                        <InputStyled type="email" placeholder='yourmail@gmail.com' value={email} onChange={e => setEmail(e.target.value)} />
+                    </InputBoxStyled>
+                    <ButtonWrapper>
+                        Next
+                        <img src='/images/arrow_forward.png' />
+                    </ButtonWrapper>
+                    <TermStyled>By entering your number you are agreeing to our Terms of services and Privacy Policy Thanks!</TermStyled>
+                </CardStyled>
+            </MainStyled>}
         </>
 
     )
---
+++ frontend/src/components/Steps/StepPhoneEmail/StepPhoneEmail.styled.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const EmailPhoneWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  position: absolute;
+  top: 75px;
+  right: 400px;
+`;
+
+export const TermStyled = styled.span`
+  color: #d9d9d9;
+  text-align: center;
+  font-weight: normal;
+  font-size: 12px;
+  opacity: 0.8;
+  padding: 0 60px;
+  width: 400px;
+`;
+export const InputBoxStyled = styled.div`
+  position: relative;
+  img {
+    position: absolute;
+    top: 8px;
+    left: 8px;
+  }
+`;
+export const InputStyled = styled.input`
+  background-color: #262626;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  padding: 12px;
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
