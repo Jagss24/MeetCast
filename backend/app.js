c36188f9a2e836e4675342bf3eb0bd90f7535709
jagannathFyntune <btwitsjagannath12@gmail.com>
2024-06-03 21:45:55 +0530

    cofigured cors library to accept data from client-side

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import conn from "./conn.js";
+import cors from "cors";
 const mongoURI = process.env.MONGO_URI;
 conn(mongoURI);
 import authenticate from "./routes/authenticate.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 app.use("/authenticate", authenticate);
 app.get("/", (req, res) => {
