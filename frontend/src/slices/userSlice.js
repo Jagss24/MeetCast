63a80f339e03115c1c3d318c1bd2aa8f95592d62
jagannathFyntune <btwitsjagannath12@gmail.com>
2024-06-03 23:39:53 +0530

    configuration of user & otp store

---
+++ frontend/src/slices/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuth: false,
+  otp: {
+    number: "",
+    hash: "",
+  },
+  user: {},
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setAuth: (state, action) => {
+      state.isAuth = action.payload;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setOtp: (state, action) => {
+      state.otp = action.payload;
+    },
+  },
+});
+
+export const { setAuth, setOtp, setUser } = userSlice.actions;
+
+export default userSlice.reducer;
