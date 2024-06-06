fdebe3746302dcda0af60d0301569df2a398a31f
jagannathFyntune <btwitsjagannath12@gmail.com>
2024-06-06 23:42:09 +0530

    if page is refreshed fetch the data and store in redux

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import "./Navigation.styled.css"
 import { HeadingImg } from '../commonStyles/Card.styled'
+import { useQuery } from '@tanstack/react-query'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from '../../../slices/userSlice'
+import { getUSer } from '../../../api/api'
 
 const Navigation = () => {
+    const dispatch = useDispatch()
+    const { user } = useSelector(state => state.user)
+    const searchParams = new URLSearchParams(window.location.search);
+    const userId = searchParams.get('user');
+    const { data, refetch } = useQuery({
+        queryKey: [userId],
+        queryFn: () => getUSer(userId),
+        retry: 0,
+        enabled: false,
+    });
+
+    useEffect(() => {
+        if (userId !== user?.id && userId) {
+            refetch()
+
+        }
+    }, [window.location.search, user?.id])
+
+    useEffect(() => {
+        if (data) {
+            if (data?.data?.message === "Not Found") {
+                window.location.assign("/");
+            }
+            else {
+                dispatch(setUser(data?.data?.userData))
+            }
+        }
+    }, [data])
+
     return (
         <nav className={`navbar container`}>
             <Link to="/" className="logo_wrapper" >
