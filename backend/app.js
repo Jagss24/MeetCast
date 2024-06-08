aa5db60e54ff02513a10cc3d9002e04a9d5677a5
jagannathFyntune <btwitsjagannath12@gmail.com>
2024-06-08 18:26:55 +0530

    to sent cookies in clientSide & activate url configuration

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(
   cors({
+    credentials: true,
     origin: process.env.FRONTEND_URL,
   })
 );
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
@@ -15,4 +16,6 @@ export const verifyOtp = (payload) =>
 
 export const getUSer = (userId) =>
   api.get(`authenticate/getUser?userId=${userId}`);
+
+export const activate = (data) => api.post("authenticate/activate", data);
 export default api;
