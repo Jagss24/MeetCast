f3311aff4e9a3f12d2a94ac2a82e87c6de703b24
Jagss24 <btwitsjagannath12@gmail.com>
2024-06-13 23:25:52 +0530

    style the roomnav component

--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { RoomComponent, RoomMainComponent } from './Rooms.styled'
+import { RoomComponent, RoomMainComponent, SearchInput, RoomNav, FirstChild, SecondChild, StartContainer } from './Rooms.styled'
+import { MdOutlinePeople } from "react-icons/md";
 
 const Rooms = () => {
     return (
         <RoomComponent>
-            <RoomMainComponent>Rooms</RoomMainComponent>
-
+            <RoomNav>
+                <FirstChild>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                        <span>All Voice rooms</span>
+                        <span style={{ border: "1px solid red", marginTop: "8px", width: "50px" }}></span>
+                    </div>
+                    <SearchInput type='text' placeholder='Type here / to search' />
+                </FirstChild>
+                <SecondChild>
+                    <MdOutlinePeople color='#fff' size={18} />
+                    <StartContainer>Start a room</StartContainer>
+                </SecondChild>
+            </RoomNav>
         </RoomComponent>
     )
 }
--- frontend/src/pages/Rooms/Rooms.styled.js
+++ frontend/src/pages/Rooms/Rooms.styled.js
@@ -2,9 +2,47 @@ import styled from "styled-components";
 
 export const RoomComponent = styled.div`
   display: flex;
+  padding-top: 80px;
+`;
+export const RoomNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  padding: 0 30px;
+`;
+
+export const FirstChild = styled.div`
+  display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  gap: 20px;
+`;
+export const SecondChild = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #20bd5f;
+  border-radius: 20px;
+  padding: 8px 12px;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+export const StartContainer = styled.button`
+  background-color: #20bd5f;
+  color: #fff;
+  border: none;
+  outline: none;
+`;
+export const SearchInput = styled.input`
+  padding: 8px 12px;
+  border-radius: 20px;
+  border: none;
+  outline: none;
+  color: #fff;
+  background-color: #353535;
 `;
 
 export const RoomMainComponent = styled.div``;
