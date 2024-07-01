6707e10f15a9f0b07cad2ab613872d9301afbabd
Jagss24 <btwitsjagannath12@gmail.com>
2024-07-01 23:20:54 +0530

    Added some new routes

--- backend/routes/authenticate.js
+++ backend/routes/authenticate.js
@@ -1,23 +1,26 @@
 import express from "express";
 import {
-  authentiCateOtpMobile,
-  verifyOtpMobile,
+  authenticateOtpEmail,
+  verifyOtpEmail,
   getUser,
   activateUser,
   autoReLoginFunctionality,
+  loginUser,
   logoutFunctionality,
 } from "../controllers/auth-controller.js";
 import { authMiddleWarefunc } from "../middlewares/authMiddleWare.js";
 
 const router = express.Router();
 
-router.post("/sendOtp", authentiCateOtpMobile);
+router.post("/sendOtp", authenticateOtpEmail);
 
-router.post("/verifyOtp", verifyOtpMobile);
+router.post("/verifyOtp", verifyOtpEmail);
 
 router.get("/getUser", getUser);
 
 router.post("/activate", authMiddleWarefunc, activateUser);
+
+router.post("/login", loginUser);
 
 router.get("/autoReLogin", autoReLoginFunctionality);
 
