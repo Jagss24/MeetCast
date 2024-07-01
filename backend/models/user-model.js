53c9582e6562bd4d20b9afb652a2a37dc94e5714
Jagss24 <btwitsjagannath12@gmail.com>
2024-07-01 23:02:05 +0530

    removed sms functionality & updated user model

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    number: {
+    emailId: {
       type: String,
       required: true,
     },
@@ -11,7 +11,13 @@ const userSchema = new mongoose.Schema(
       required: true,
       default: false,
     },
-    name: {
+    userName: {
+      type: String,
+    },
+    fullName: {
+      type: String,
+    },
+    password: {
       type: String,
     },
     avatar: {
--- backend/services/otpServices.js
+++ backend/services/otpServices.js
@@ -1,5 +1,4 @@
 import crypto from "crypto";
-import twilio from "twilio";
 
 export const generateOtp = async () => {
   return crypto.randomInt(1000, 9999);
@@ -11,17 +10,3 @@ export const hashOtp = async (data) => {
     .update(data)
     .digest("hex");
 };
-
-export const sendSms = async (phone, otp) => {
-  const smsSid = process.env.SMS_SID;
-  const smsAuthToken = process.env.SMS_AUTH_TOKEN;
-
-  const twilioConfig = twilio(smsSid, smsAuthToken);
-  return await twilioConfig.messages.create({
-    to: phone,
-    from: process.env.SMS_FROM_NUMBER,
-    body: `Your Voicehub Signup OTP is ${otp}`,
-  });
-};
-
-// export const sentMail = ()
