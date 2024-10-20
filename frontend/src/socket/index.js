ef64f5ae880b4af2de52ceb5172ff8acea388af5
Jagss24 <btwitsjagannath12@gmail.com>
2024-10-20 19:48:21 +0530

    temp sol

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -9,5 +9,5 @@ export const socketInit = () => {
     path: "/api/socket",
   };
 
-  return io(import.meta.env.VITE_BACKEND_URL, options);
+  return io(import.meta.env.VITE_BACKEND_URL, { path: "/api/socket" });
 };
