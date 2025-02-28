433d16c6f7d1634750b6964296bcd103dcdb20a4
Jagss24 <btwitsjagannath12@gmail.com>
2025-03-01 00:22:33 +0530

    style: made this styles unique

--- frontend/src/components/shared/commonStyles/Card.styled.js
+++ frontend/src/components/shared/commonStyles/Card.styled.js
@@ -77,3 +77,32 @@ export const ButtonWrapper = styled.button`
     border: 3px solid #20bd5f;
   }
 `;
+
+export const TermStyled = styled.span`
+  color: #d9d9d9;
+  text-align: center;
+  font-weight: normal;
+  font-size: 0.75rem;
+  opacity: 0.8;
+  padding: 0 3.75rem;
+  white-space: nowrap;
+`;
+
+export const InputStyled = styled.input`
+  position: relative;
+  background-color: #ffffff;
+  border-radius: 1.25rem;
+  border: none;
+  outline: none;
+  padding: 0.5rem 0.625rem 0.5rem 1.875rem;
+  color: #334155;
+  font-size: 1rem;
+  &::placeholder {
+    color: #a1a1aa;
+  }
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
